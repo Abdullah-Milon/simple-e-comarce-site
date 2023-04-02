c599c40bb01ba20a3fbd171614f6d0400990e59c
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-04-02 20:23:18 +0600

    no definit message

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,24 +3,36 @@ import './Product.css'
 
 // const Product = (props.product) => {
 // const {img, name, seller, quantity, price} = props.product
-const Product = ({ product }) => {
-    return (
-        <div className='product '>
-            <img src={product.img} alt="" />
-            <div className='p-details'>
-                <h4>{product.name}</h4>
-                <h5>Price:{product.price}</h5>
-                <div className="p-info">
-                    <p>Manufecturar:  {product.seller}</p>
-                    <p>Rating: {product.ratings} star</p>
-                </div>
-            </div>
-            <div>
-                <button className='btn-cart'>Add to cart</button>
+//const handleToCart = props.handleToCart;
+
+const Product = ({ product, handleAddToCart }) => {
+
+    /* ami chaile handleCart fn a parameter hiseeb k j kno name a 
+    accept korte pari tobe sob theke vlo projetct related name newa tai 
+    niche nisi 
+    const handleAddToCart = (diboNa) =>{
+         console.log(diboNa)
+     } */
+
+    //3
+
+return (
+    <div className='product '>
+        <img src={product.img} alt="" />
+        <div className='p-details'>
+            <h4>{product.name}</h4>
+            <h5>Price:{product.price}</h5>
+            <div className="p-info">
+                <p>Manufecturar:  {product.seller}</p>
+                <p>Rating: {product.ratings} star</p>
             </div>
         </div>
+        <div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>Add to cart</button>
+        </div>
+    </div>
 
-    );
+);
 };
 
 export default Product;
